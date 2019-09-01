38bc4b49f539826a345184e85ed38271ae03491f
Laurent Bassin <laurent@bassin.info>
2019-09-01 15:51:44 -0400

    New employee

---
+++ src/containers/employee/EmployeeForm.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+const inputStyle = {
+    marginBottom: '12px'
+};
+
+class EmployeeForm extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            test: '',
+            role: ''
+        }
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+
+        // TODO Data validation
+
+        this.props.onSubmit(this.state);
+    }
+
+    handleEdit = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.submitForm}>
+                <div style={inputStyle}>
+                    <label>
+                        Name <br/>
+                        <input name="name" type="text" onKeyUp={this.handleEdit}/>
+                    </label>
+                </div>
+
+                <div style={inputStyle}>
+                    <label>
+                        Role <br/>
+                        <input name="role" type="text" onKeyUp={this.handleEdit}/>
+                    </label>
+                </div>
+
+                <div style={inputStyle}>
+                    <button>Add employee</button>
+                </div>
+            </form>
+        );
+    }
+}
+
+export default EmployeeForm;
--- src/containers/employee/EmployeeNew.js
+++ src/containers/employee/EmployeeNew.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import EmployeeForm from '../employee/EmployeeForm';
+import EmployeeRepository from "../../repositories/EmployeeRepository";
+
+const formStyle = {
+    width: '80%',
+    maxWidth: '650px',
+    textAlign: 'center',
+    border: '1px solid #000',
+    padding: '6px',
+    margin: 'auto'
+};
 
 class EmployeeNew extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            employeeCreated: false
+        }
+    }
+
+    handleSubmit = (employee) => {
+        EmployeeRepository.create(employee);
+
+        this.setState({
+            employeeCreated: true
+        });
+    }
+
     render() {
         return (
-            <div>
-                Employee New
+            <div style={formStyle}>
+                <p>Add an employee</p>
+                <EmployeeForm onSubmit={this.handleSubmit}/>
+                {this.state.employeeCreated && <p>Employee successfully created</p>}
             </div>
         );
     }
